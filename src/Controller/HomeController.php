67cf23813cc3cc03cd2587a7b55477fd60fd5f89
Waut <sebastien.farese@gmail.com>
2021-01-09 11:50:30 +0100

    initial commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,12 +9,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
+    public function landing(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/menu", name="index")
+     */
+
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index2.html.twig');
     }
 }
